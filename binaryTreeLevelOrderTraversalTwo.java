c3d12d3fef01bc8c9d7170a60fcb3916bddde7cc
dibyo20 <dibyobanerjee005@gmail.com>
2026-03-29 15:59:42 +0530

    binaryTreeLevelOrderTraversalTwo

---
+++ binaryTreeLevelOrderTraversalTwo.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+public class binaryTreeLevelOrderTraversalTwo {
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            result.addFirst(level); // This is the change: add to the front of the list instead of the end.
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        List<List<Integer>> result = levelOrderBottom(root);
+        System.out.println(result);
+    }
+}
