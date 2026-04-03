34ba765e03323d9e6b602b16fffed93cb047846c
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-03 14:31:28 +0530

    constructBinaryTreeFromPreorderAndInorder

---
+++ constructBinaryTreeFromPreorderAndInorder.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class constructBinaryTreeFromPreorderAndInorder {
+    static int preIndex = 0;
+    static HashMap<Integer, Integer> map = new HashMap<>();
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return construct(preorder, 0, inorder.length - 1);
+    }
+
+    private static TreeNode construct(int[] preorder, int left, int right) {
+        if (left > right)
+            return null;
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = map.get(rootVal);
+        root.left = construct(preorder, left, index - 1);
+        root.right = construct(preorder, index + 1, right);
+        return root;
+    }
+
+    public static void printInorder(TreeNode root) {
+        if (root == null)
+            return;
+        printInorder(root.left);
+        System.out.print(root.val + " ");
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = { 3, 9, 20, 15, 7 };
+        int[] inorder = { 9, 3, 15, 20, 7 };
+        TreeNode root = buildTree(preorder, inorder);
+        printInorder(root);
+    }
+}
