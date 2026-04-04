ad0472cad88d467447fd4c9c27131c9c01eaca7c
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-04 13:38:59 +0530

    convertSortedArrayToBinarySearchTree

---
+++ convertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class convertSortedArrayToBinarySearchTree {
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right)
+            return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
+        return root;
+    }
+
+    public static void printInorder(TreeNode root) {
+        if (root == null)
+            return;
+        printInorder(root.left);
+        System.out.print(root.val + " ");
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -10, -3, 0, 5, 9 };
+        TreeNode root = sortedArrayToBST(nums);
+        printInorder(root);
+    }
+}
