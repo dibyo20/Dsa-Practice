a87eb4d181a5effb1deaf7f6a277ad88133ce8ef
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-06 01:28:19 +0530

    invertBinaryTree

---
+++ invertBinaryTree.java
@@ -0,0 +1,45 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
+
+public class invertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        root = invertTree(root);
+        inorder(root);
+    }
+}
