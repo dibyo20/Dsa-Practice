3d6968e101573aa292fb92520a39022d1fea1a00
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-11 12:12:32 +0530

    numberOfIslands

---
+++ numberOfIslands.java
@@ -0,0 +1,48 @@
+public class numberOfIslands {
+    public static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        // explore 4 directions
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        System.out.println(numIslands(grid));
+    }
+}
