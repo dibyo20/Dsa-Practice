5daa2e9fa99519d38a6a1cfdd10f02d2856fe331
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-12 10:13:55 +0530

    maxAreaOfIsland

---
+++ maxAreaOfIsland.java
@@ -0,0 +1,47 @@
+public class maxAreaOfIsland {
+    public static int maxArea(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int maxArea = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    public static int dfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+
+        return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+        };
+        System.out.println(maxArea(grid));
+    }
+}
