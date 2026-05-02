138bad217c7814d9dc20739e8d4511bba7e0341c
dibyo20 <dibyobanerjee005@gmail.com>
2026-05-02 14:33:06 +0530

    subarrayProductLessThanK

---
+++ Two Pointers/subarrayProductLessThanK.java
@@ -0,0 +1,27 @@
+public class subarrayProductLessThanK {
+    public static int countSubarray(int nums[], int k) {
+        if (k <= 1)
+            return 0;
+
+        int left = 0;
+        int product = 1;
+        int count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+
+            while (product >= k) {
+                product /= nums[left];
+                left++;
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 10, 5, 2, 6 };
+        int k = 100;
+        System.out.println(countSubarray(nums, k));
+    }
+}
