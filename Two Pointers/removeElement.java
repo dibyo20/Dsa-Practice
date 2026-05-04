f7f096ab64503f0f061da7d6dc20e77721693fb9
dibyo20 <dibyobanerjee005@gmail.com>
2026-05-04 14:44:51 +0530

    removeElement

---
+++ Two Pointers/removeElement.java
@@ -0,0 +1,18 @@
+public class removeElement {
+    public static int removeEle(int nums[], int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 3, 2, 2, 3 };
+        int val = 3;
+        System.out.println(removeEle(nums, val));
+    }
+}
