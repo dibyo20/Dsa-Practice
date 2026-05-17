affe407fb1f3986f06a34a924039eacbe8abbbbe
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-05-17 10:31:59 +0530

    subarraySumEqualsK

---
+++ Prefix Sum/subarraySumEqualsK.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class subarraySumEqualsK {
+    public static int subarraySum(int nums[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3 };
+        int k = 3;
+        System.out.println(subarraySum(nums, k));
+    }
+}
