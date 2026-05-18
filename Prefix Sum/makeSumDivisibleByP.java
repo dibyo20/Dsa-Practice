91e1ac857e56bc1ab93a6710c518a81e89b0b229
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-05-18 10:42:25 +0530

    makeSumDivisibleByP

---
+++ Prefix Sum/makeSumDivisibleByP.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class makeSumDivisibleByP {
+    public static int minSubarray(int[] nums, int p) {
+        long total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        int target = (int) (total % p);
+        if (target == 0) {
+            return 0;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int prefix = 0;
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (prefix + nums[i]) % p;
+            int rem = (prefix - target + p) % p;
+            if (map.containsKey(rem)) {
+                res = Math.min(res, i - map.get(rem));
+            }
+            map.put(prefix, i);
+        }
+        return res == nums.length ? -1 : res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 1, 4, 2 };
+        int p = 6;
+        System.out.println(minSubarray(nums, p));
+    }
+}
