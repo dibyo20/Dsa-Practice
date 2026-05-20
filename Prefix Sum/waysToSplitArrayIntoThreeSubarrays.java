76155463aec3bf195d95ce3e9389c37342f5ddc2
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-05-20 10:32:34 +0530

    waysToSplitArrayIntoThreeSubarrays

--- Prefix Sum/waysToSplitArrayIntoThreeSubarrays.java
+++ Prefix Sum/waysToSplitArrayIntoThreeSubarrays.java
@@ -11,15 +11,15 @@ public class waysToSplitArrayIntoThreeSubarrays {
         int j = 0, k = 0;
         for (int i = 0; i < n - 2; i++) {
             j = Math.max(j, i + 1);
-            while (j < n - 1 && prefix[j] < 2 * prefix[i]) {
+            while (j < n - 1 && prefix[j] < 2 * prefix[i]) { // Here we are checking if the sum of the first subarray is less than the sum of the second subarray by the formula 2 * prefix[i] because the sum of the first subarray is prefix[i] and the sum of the second subarray is prefix[j] - prefix[i], so we can rearrange the formula to get prefix[j] < 2 * prefix[i].
                 j++;
             }
-            k = Math.max(k, j);
-            while (k < n - 1 && prefix[k] <= (prefix[n - 1] + prefix[i]) / 2) {
+            k = Math.max(k, j); // Here we are checking if the sum of the second subarray is less than the sum of the third subarray by the formula (prefix[n - 1] + prefix[i]) / 2 because the sum of the second subarray is prefix[j] - prefix[i] and the sum of the third subarray is prefix[n - 1] - prefix[j], so we can rearrange the formula to get prefix[j] < (prefix[n - 1] + prefix[i]) / 2.
+            while (k < n - 1 && prefix[k] <= (prefix[n - 1] + prefix[i]) / 2) { // Here we are checking if the sum of the second subarray is less than or equal to the sum of the third subarray by the formula (prefix[n - 1] + prefix[i]) / 2 because the sum of the second subarray is prefix[j] - prefix[i] and the sum of the third subarray is prefix[n - 1] - prefix[j], so we can rearrange the formula to get prefix[j] <= (prefix[n - 1] + prefix[i]) / 2.
                 k++;
             }
             if (j < k) {
-                result = (result + (k - j)) % mod;
+                result = (result + (k - j)) % mod; // Here we are doing mod, because the result can be very large, so we need to take the mod of the result to avoid overflow.
             }
         }
         return result;
