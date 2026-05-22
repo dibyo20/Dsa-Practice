cdc04cd318aef2a6e4a87004dbad7f624d01b890
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-05-22 10:05:16 +0530

    findMinimumInRotatedSortedArray

---
+++ Binary Search/findMinimumInRotatedSortedArray.java
@@ -0,0 +1,24 @@
+public class findMinimumInRotatedSortedArray {
+    public static int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+                continue;
+            }
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, 5, 1, 2 };
+        System.out.println(findMin(nums));
+    }
+}
