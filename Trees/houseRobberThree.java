bdcdce9a98a0405135e90302415836a2b034743e
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-05-26 20:51:57 +0530

    houseRobberThree

---
+++ Trees/houseRobberThree.java
@@ -0,0 +1,48 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class houseRobberThree {
+    public static int rob(TreeNode root) {
+        int[] result = dfs(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    private static int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[] { 0, 0 };
+        }
+
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+
+        int notRob = Math.max(left[0], left[1])
+                + Math.max(right[0], right[1]);
+
+        int rob = node.val + left[0] + right[0];
+
+        return new int[] { notRob, rob };
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(1);
+
+        System.out.println("Maximum money robbed = " + rob(root));
+    }
+}
