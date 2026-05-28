2d2ea211aefaf9016fef3b4f091ebc8db3f8a50b
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-05-28 10:23:27 +0530

    kthSmallestElementInBST

---
+++ Trees/kthSmallestElementInBST.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class kthSmallestElementInBST {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        int count = 0;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            count++;
+
+            if (count == k) {
+                return curr.val;
+            }
+
+            curr = curr.right;
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(1);
+
+        System.out.println(kthSmallest(root, 3));
+    }
+}
