86d823f1322d77589422b255a64e3f126a2bd91c
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-06-01 10:47:59 +0530

    couseScheduleTwo

---
+++ Graph/couseScheduleTwo.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class couseScheduleTwo {
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+
+            graph.get(prereq).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int[] result = new int[numCourses];
+        int index = 0;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            result[index++] = curr;
+
+            for (int neighbor : graph.get(curr)) {
+                indegree[neighbor]--;
+
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        if (index == numCourses) {
+            return result;
+        }
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int numCourses = 4;
+        int[][] prerequisites = { { 1, 0 }, { 2, 0 }, { 3, 1 }, { 3, 2 } };
+        System.out.println(Arrays.toString(findOrder(numCourses, prerequisites)));
+    }
+}
