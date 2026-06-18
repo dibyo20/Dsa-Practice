f85d6e4af79d9918070950d34eccd59385093225
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-06-18 10:23:36 +0530

    AngleBetweenHandsOfAClock

---
+++ DSA Daily Practice Questions/AngleBetweenHandsOfAClock.java
@@ -0,0 +1,12 @@
+public class AngleBetweenHandsOfAClock {
+    public static double angleClock(int hour, int minutes) {
+        double min_angle = minutes * 6.0;
+        double hour_angle = (hour % 12) * 30.0 + minutes * 0.5;
+        double angle = Math.abs(hour_angle - min_angle);
+        return Math.min(angle, 360 - angle);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(angleClock(12, 30));
+    }
+}
