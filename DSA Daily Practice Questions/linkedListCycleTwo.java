521c7a505d20d23a7a915332bcb71130faa7adc4
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-06-25 23:23:26 +0530

    linkedListCycleTwo

---
+++ DSA Daily Practice Questions/linkedListCycleTwo.java
@@ -0,0 +1,47 @@
+public class linkedListCycleTwo {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean cycle = false;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next;
+
+        ListNode ans = detectCycle(head);
+        System.out.println(ans.val);
+    }
+}
