e5446b030782b1c7959c1aa131e18db9cc7e149e
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-07-10 12:04:42 +0530

    flattenBinaryTreeToLinkedList

---
+++ DSA Daily Practice Questions/flattenBinaryTreeToLinkedList.java
@@ -0,0 +1,49 @@
+public class flattenBinaryTreeToLinkedList {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void flatten(TreeNode root) {
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode prev = curr.left;
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+
+        flatten(root);
+        printTree(root);
+    }
+}
