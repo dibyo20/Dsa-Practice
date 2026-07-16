671095aeef2be4cee85430270615956096ceea9a
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-07-16 11:55:42 +0530

    oddEvenLinkedList

---
+++ DSA Daily Practice Questions/oddEvenLinkedList.java
@@ -0,0 +1,46 @@
+public class oddEvenLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode oddEven(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    public static void main(String args[]) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode ans = oddEven(head);
+        while (ans != null) {
+            System.out.println(ans.val);
+            ans = ans.next;
+        }
+    }
+}
