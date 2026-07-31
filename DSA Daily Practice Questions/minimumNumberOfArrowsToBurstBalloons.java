efc48c0e56f141768ba0526153c6c957779addff
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-07-31 19:33:21 +0530

    minimumNumberOfArrowsToBurstBalloons

---
+++ DSA Daily Practice Questions/minimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class minimumNumberOfArrowsToBurstBalloons {
+    public static int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int arrows = 1;
+        int arrowPosition = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > arrowPosition) {
+                arrows++;
+                arrowPosition = points[i][1];
+            }
+        }
+        return arrows;
+    }
+
+    public static void main(String[] args) {
+        int[][] points = { { 10, 16 }, { 2, 8 }, { 1, 6 }, { 7, 12 } };
+        System.out.println(findMinArrowShots(points));
+    }
+}
