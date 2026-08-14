0fdcb359c2d49f553497c469772f0c821088cf99
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-14 12:05:01 +0530

    determineIfTwoStringsAreClose

---
+++ Basic Foundation Patterns/HashMap/determineIfTwoStringsAreClose.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class determineIfTwoStringsAreClose {
+    public static boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0) != (freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        return Arrays.equals(freq1, freq2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(closeStrings("abc", "bca"));
+        System.out.println(closeStrings("a", "aa"));
+        System.out.println(closeStrings("cabbba", "abbccc"));
+    }
+}
