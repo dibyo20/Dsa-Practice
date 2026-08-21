f339c3773858e6e1f8fc1395b56f0cdf71911000
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-21 10:28:54 +0530

    countSubmatricesWithAllOnes

---
+++ Basic Foundation Patterns/Arrays/countSubmatricesWithAllOnes.java
@@ -0,0 +1,34 @@
+public class countSubmatricesWithAllOnes {
+    public static int countSubmatricesWithlOnes(int[][] matrix) {
+        int[] heights = new int[matrix[0].length];
+        int ans = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 1) {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+
+            for (int j = 0; j < matrix[0].length; j++) {
+                int minHeight = Integer.MAX_VALUE;
+                for (int k = j; k >= 0; k--) {
+                    minHeight = Math.min(minHeight, heights[k]);
+                    ans += minHeight;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 0, 1 },
+                { 1, 1, 0 },
+                { 1, 1, 0 }
+        };
+        System.out.println(countSubmatricesWithlOnes(matrix));
+    }
+}
