0571897ba2dd8c2cae63a7779bac00ff02fa1d6c
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-26 11:03:39 +0530

    evaluateReversePolishNotation

---
+++ Core Data Structures/Stacks/evaluateReversePolishNotation.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class evaluateReversePolishNotation {
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int divisor = stack.pop();
+                int dividend = stack.pop();
+                stack.push(dividend / divisor);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String tokens[] = { "2", "1", "+", "3", "*" };
+        System.out.println(evalRPN(tokens));
+    }
+}
