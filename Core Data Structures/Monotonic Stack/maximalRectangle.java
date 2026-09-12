9eb07cd4ba6ee8f17ce82b2a0b7993753b5c4f06
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-12 12:13:57 +0530

    maximalRectangle

---
+++ Core Data Structures/Monotonic Stack/maximalRectangle.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class maximalRectangle {
+    public static int maxRectangle(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+        return maxArea;
+    }
+
+    private static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for (int i = 0; i <= heights.length; i++) {
+            int currHeight = (i == heights.length) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width;
+
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = {
+                { '1', '0', '1', '0', '0' },
+                { '1', '0', '1', '1', '1' },
+                { '1', '1', '1', '1', '1' },
+                { '1', '0', '0', '1', '0' }
+        };
+        System.out.println(maxRectangle(matrix));
+    }
+}
