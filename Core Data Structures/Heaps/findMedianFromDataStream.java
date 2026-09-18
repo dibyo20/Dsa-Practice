b5165f7ab93af8be8ff632af737b62385467ac7f
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-18 10:12:29 +0530

    findMedianFromDataStream

---
+++ Core Data Structures/Heaps/findMedianFromDataStream.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class findMedianFromDataStream {
+    public static class MedianFinder {
+        public PriorityQueue<Integer> minHeap;
+        public PriorityQueue<Integer> maxHeap;
+
+        public MedianFinder() {
+            minHeap = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        }
+
+        public void addNum(int num) {
+            if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+                maxHeap.offer(num);
+            } else {
+                minHeap.offer(num);
+            }
+
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.offer(maxHeap.poll());
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.offer(minHeap.poll());
+            }
+        }
+
+        public double findMedian() {
+            if (maxHeap.size() == minHeap.size()) {
+                return (maxHeap.peek() + minHeap.peek()) / 2.0;
+            } else {
+                return maxHeap.peek();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MedianFinder mf = new MedianFinder();
+        mf.addNum(1);
+        mf.addNum(2);
+        System.out.println(mf.findMedian());
+        mf.addNum(3);
+        System.out.println(mf.findMedian());
+    }
+}
