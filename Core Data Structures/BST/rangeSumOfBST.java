5c06dcefb701430e74e610177acc5129b77d339b
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-20 11:06:02 +0530

    rangeSumOfBST

---
+++ Core Data Structures/BST/rangeSumOfBST.java
@@ -0,0 +1,36 @@
+public class rangeSumOfBST {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return 0;
+
+        if (root.val >= low && root.val <= high) {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        } else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else {
+            return rangeSumBST(root.left, low, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.right.right = new TreeNode(18);
+        System.out.println(rangeSumBST(root, 7, 15));
+    }
+}
