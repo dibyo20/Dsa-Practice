26a5353fdafff2f299807e6299ee49b4aad10fe9
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-21 23:37:05 +0530

    firstMatchingCharacterFromBothEnds

---
+++ DSA Daily Practice Questions/firstMatchingCharacterFromBothEnds.java
@@ -0,0 +1,17 @@
+public class firstMatchingCharacterFromBothEnds {
+    public static int firstMatch(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                return left;
+            }
+            left++;
+            right--;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstMatch("abccbd"));
+    }
+}
