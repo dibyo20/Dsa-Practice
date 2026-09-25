1cb04e9b97f81e5bc1adae7cdc62246c7e0263b9
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-25 11:19:16 +0530

    binaryTreePruning

---
+++ Core Data Structures/Binary Trees/binaryTreePruning.java
@@ -0,0 +1,50 @@
+public class binaryTreePruning {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.val == 0 && root.left == null && root.right == null) {
+            return null;
+        }
+
+        return root;
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(0);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(1);
+
+        inorder(root);
+        System.out.println();
+        pruneTree(root);
+        inorder(root);
+    }
+}
