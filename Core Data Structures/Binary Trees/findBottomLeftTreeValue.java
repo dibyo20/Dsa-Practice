23524002a6d940c6330f5bcbb77955064b10ad28
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-26 11:13:15 +0530

    findBottomLeftTreeValue

---
+++ Core Data Structures/Binary Trees/findBottomLeftTreeValue.java
@@ -0,0 +1,46 @@
+public class findBottomLeftTreeValue {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int ans = 0;
+    public static int maxLevel = 0;
+
+    public static int findBottomLeftValue(TreeNode root) {
+        dfs(root, 0);
+        return ans;
+    }
+
+    private static void dfs(TreeNode root, int level) {
+        if (root == null)
+            return;
+
+        if (level > maxLevel) {
+            maxLevel = level;
+            ans = root.val;
+        }
+
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.right.left.left = new TreeNode(7);
+
+        System.out.println(findBottomLeftValue(root));
+    }
+}
